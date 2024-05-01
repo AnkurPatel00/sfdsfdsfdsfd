f0503547a67c1515fcc0fa796a49e9c5e16e7a9a
Ankur Patel <anpatel@gsngames.com>
2024-05-01 10:31:35 +0530

    added audio related scripts

---
+++ Card Match/Assets/Scripts/Audio/API/IAudioPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Game.CommonModules.Pooling.Audio
+{
+    public interface IAudioPlayer
+    {
+        /// <summary>
+        /// Play particular clip, with given volume
+        /// </summary>
+        /// <param name="clip"></param>
+        /// <param name="volume"></param>
+        void Play(AudioClip clip, float volume = 1);
+        
+        /// <summary>
+        /// Pause Audio
+        /// </summary>
+        void Pause();
+        
+        /// <summary>
+        /// UnPause Audio
+        /// </summary>
+        void UnPause();
+    }
+}
---
+++ Card Match/Assets/Scripts/Audio/Implementation/AudioPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Game.CommonModules.Pooling.Audio
+{
+    public class AudioPlayer : IAudioPlayer
+    {
+        private AudioSource pAudioSource;
+
+        private AudioSource _audioSource
+        {
+            get
+            {
+                if (pAudioSource == null)
+                    pAudioSource = Camera.main.GetComponent<AudioSource>();
+                return pAudioSource;
+            }
+        }
+
+        public void Play(AudioClip clip, float volume = 1)
+        {
+            _audioSource.PlayOneShot(clip, volume);
+        }
+
+        public void Pause()
+        {
+            _audioSource.Pause();
+        }
+
+        public void UnPause()
+        {
+            _audioSource.UnPause();
+        }
+    }
+}
