fa4321f3440f5d2f196ac90c304b8f5fc7f492c8
Ankur Patel <anpatel@gsngames.com>
2024-05-01 11:24:20 +0530

    added GameModel script for game save and load

---
+++ Card Match/Assets/Scripts/Game/GameModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Model
+{
+    [Serializable]
+    public class CardStorageHolder
+    {
+        public List<CardDataStorage> cardDataStorage;
+
+        public CardStorageHolder(List<CardDataStorage> cardDataStorages)
+        {
+            cardDataStorage = cardDataStorages;
+        }
+    }
+
+    [Serializable]
+    public class CardDataStorage
+    {
+        public Vector2 coord;
+        public int cardIndex;
+
+        public CardDataStorage(Vector2 coord, int cardIndex)
+        {
+            this.coord = coord;
+            this.cardIndex = cardIndex;
+        }
+    }
+}
